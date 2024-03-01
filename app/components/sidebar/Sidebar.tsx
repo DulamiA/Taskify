d1b8828097db8f42a60b7753a625db398e3b6ebf
DulamiA <119730845+DulamiA@users.noreply.github.com>
2024-03-01 16:51:07 +0530

    sidebar

---
+++ app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,19 @@
+"use client"
+import React from 'react'
+import styled from 'styled-components'
+import {useGlobalState} from "@/app/context/globalProvider";
+
+function Sidebar() {
+  const {theme}= useGlobalState();
+
+  return (
+    <SidebarStyled theme={theme}>sidebar</SidebarStyled>
+  )
+}
+
+const SidebarStyled = styled.nav`
+    background-color: white;
+    width: ${(props) => props.theme.sidebarWidth};
+`
+
+export default Sidebar
